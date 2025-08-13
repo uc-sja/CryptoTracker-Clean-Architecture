793b37be35b7f60fa52b10e2d3db38eb284a79b0
uc-sja <shikharj.mobcoder@gmail.com>
2025-08-13 16:27:07 +0530

    setup list screen of coins

---
+++ app/src/main/java/com/shikhar/cryptochecker/crypto/presentation/coin_list/CoinListState.kt
@@ -0,0 +1,11 @@
+package com.shikhar.cryptochecker.crypto.presentation.coin_list
+
+import androidx.compose.runtime.Immutable
+import com.shikhar.cryptochecker.crypto.presentation.models.CoinUi
+
+@Immutable
+data class CoinListState(
+    val isLoading: Boolean = false,
+    val coins: List<CoinUi> = emptyList(),
+    val selectedCoin: CoinUi? = null
+)
