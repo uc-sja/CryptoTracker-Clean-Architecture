6e1265b88fcc0a5b8a30e765d319da8a0eaf2333
uc-sja <shikharj.mobcoder@gmail.com>
2025-09-13 06:57:31 +0530

    fix semantics of generic Result class

--- app/src/main/java/com/shikhar/cryptochecker/core/domain/util/Result.kt
+++ app/src/main/java/com/shikhar/cryptochecker/core/domain/util/Result.kt
@@ -2,23 +2,23 @@ package com.shikhar.cryptochecker.core.domain.util
 
 typealias DomainError = Error
 
-sealed interface Result<out D, out E: Error> {
+sealed interface Result<out D, out E: DomainError> {
     data class Success<out D>(val data: D): Result<D, Nothing>
     data class Error<out E: DomainError>(val error: E): Result<Nothing, E>
 }
 
-inline fun <T, E: Error, R> Result<T, E>.map(map: (T) -> R): Result<R, E> {
+inline fun <T, E: DomainError, R> Result<T, E>.map(map: (T) -> R): Result<R, E> {
     return when(this) {
         is Result.Error -> Result.Error(error)
         is Result.Success -> Result.Success(map(data))
     }
 }
 
-fun <T, E: Error> Result<T, E>.asEmptyDataResult(): EmptyResult<E> {
+fun <T, E: DomainError> Result<T, E>.asEmptyDataResult(): EmptyResult<E> {
     return map {  }
 }
 
-inline fun <T, E: Error> Result<T, E>.onSuccess(action: (T) -> Unit): Result<T, E> {
+inline fun <T, E: DomainError> Result<T, E>.onSuccess(action: (T) -> Unit): Result<T, E> {
     return when(this) {
         is Result.Error -> this
         is Result.Success -> {
@@ -27,7 +27,7 @@ inline fun <T, E: Error> Result<T, E>.onSuccess(action: (T) -> Unit): Result<T, 
         }
     }
 }
-inline fun <T, E: Error> Result<T, E>.onError(action: (E) -> Unit): Result<T, E> {
+inline fun <T, E: DomainError> Result<T, E>.onError(action: (E) -> Unit): Result<T, E> {
     return when(this) {
         is Result.Error -> {
             action(error)
--- app/src/main/java/com/shikhar/cryptochecker/theme/Theme.kt
+++ app/src/main/java/com/shikhar/cryptochecker/theme/Theme.kt
@@ -251,8 +251,8 @@ fun CryptoTrackerTheme(
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
 
-        darkTheme -> darkScheme
-        else -> lightScheme
+        darkTheme -> highContrastDarkColorScheme
+        else -> highContrastLightColorScheme
     }
 
     MaterialTheme(
